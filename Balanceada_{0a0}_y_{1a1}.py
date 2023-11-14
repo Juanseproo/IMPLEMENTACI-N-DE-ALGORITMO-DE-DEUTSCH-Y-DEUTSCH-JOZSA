612f74f93331f8bf45994696d98a28573bb66094
Juanseproo <143037261+Juanseproo@users.noreply.github.com>
2023-11-13 23:39:25 -0500

    Primera entrega del 1er y 2do Punto
    1. Implemente las 4 funciones posibles de {0,1} a {0,1} usando el computador cuántico de IBM.
    
    Dibujo de función
    Matriz correpondiente
    Circuito correspondiente
    Resultados de las 4 pruebas
    2. Verifique que el algoritmos de Deutsch funciona para comprobar cuáles de estas funciones son balanceadas o constantes.
    
    Circuito
    Resultados

---
+++ Balanceada_{0a0}_y_{1a1}.py
@@ -0,0 +1,91 @@
+#Juan Sebastian Buitrago Piñeros
+from qiskit import QuantumCircuit, transpile
+from qiskit import Aer
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+# Inicializar el simulador, matriz y diccionario (servira mas adelante)
+simulator, matriz, ayuda_posiciones_matriz = Aer.get_backend('qasm_simulator'), [[0 for _ in range(4)] for _ in range(4)], {"00": 0, "01": 1, "10": 2, "11": 3}
+
+def Analizar_Circuito(circuit, n_circuito):
+    """
+    Analiza los circuitos dados
+    """
+    compiled_circuit = transpile(circuit, simulator)
+    job = simulator.run(compiled_circuit, shots=1000)
+    result = job.result()
+    counts = result.get_counts(circuit)
+    if n_circuito != None:
+        # Obtener la única clave del diccionario "counts"
+        clave = list(counts.keys())[0]
+        matriz[n_circuito][ayuda_posiciones_matriz[clave]] = 1
+        print("\nCircuito #", n_circuito + 1)
+    else:
+        # Dice explicitamente si es Balanceada o Constante
+        if "00" in counts.keys():
+            print("\nEs Constante")
+        else:
+            print("\nEs Balanceada")
+    print("\nTotal count for 00 and 11 are:", counts)
+    print(circuit)
+    plot_histogram(counts)
+    plt.show()
+
+# Circuito de Entrada 00
+circuit = QuantumCircuit(2, 2)
+circuit.barrier()
+circuit.cx(0,1)
+circuit.barrier()
+circuit.measure(0,1)
+circuit.measure(1,0)
+Analizar_Circuito(circuit, 0)
+
+# Circuito de Entrada 01
+circuit = QuantumCircuit(2, 2)
+circuit.x(1)
+circuit.barrier(0, 1)
+circuit.cx(0, 1)
+circuit.barrier(0, 1)
+circuit.measure(0,1)
+circuit.measure(1,0)
+Analizar_Circuito(circuit, 1)
+
+# Circuito de Entrada 10
+circuit = QuantumCircuit(2, 2)
+circuit.x(0)
+circuit.barrier(0, 1)
+circuit.cx(0, 1)
+circuit.barrier(0, 1)
+circuit.measure(0, 1)
+circuit.measure(1, 0)
+Analizar_Circuito(circuit, 2)
+
+# Circuito de Entrada 11
+circuit = QuantumCircuit(2, 2)
+circuit.x(0)
+circuit.x(1)
+circuit.barrier(0, 1)
+circuit.cx(0, 1)
+circuit.barrier(0, 1)
+circuit.measure(0,1)
+circuit.measure(1,0)
+Analizar_Circuito(circuit, 3)
+
+# Imprimir Matriz
+print("\nMatriz Correspondiente")
+for fila in matriz:
+    print(fila)
+
+# Algoritmo de Deutsch para comprobar si la funcion es balanceada o constante.
+print("\nAlgoritmo de Deutsch para comprobar si la funcion es balanceada o constante")
+circuit = QuantumCircuit(2, 2)
+circuit.h(0)
+circuit.x(1)
+circuit.h(1)
+circuit.barrier()
+circuit.cx(0,1)
+circuit.barrier()
+circuit.h(0)
+circuit.barrier()
+circuit.measure(0,0)
+Analizar_Circuito(circuit, None)
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Libreria de Algoritmo de Deutsch y Deutsch-Jozsa
+## Este repositorio contiene:
+### 1. Las 4 funciones posibles de {0,1} a {0,1} en las cuales se muestra:
+- Dibujo de función
+- Matriz correpondiente
+- Circuito correspondiente
+- Resultados de las 4 pruebas
+
+Adicionalmente se dice explicitamente si es constante o balanceada la funcion analizada. 
+
+Todo se hizo con ayuda de el computador cuántico de IBM y las librerias qiskit y matplotlib.
+
+(El código esta en los archivos: "Balanceada_{0a0}_y_{1a1}.py", "Balanceada_{0a1}_y_{1a0}.py", "Constante_{0a0}_y_{1a0}.py" y "Constante_{0a1}_y_{1a1}.py")
+
+### 2. Implementación del algoritmo de Deutsch para comprobar cuáles de las funciones del punto 1 son balanceadas o constantes.
+- Circuito
+- Resultados
+(El código esta en los archivos: "Balanceada_{0a0}_y_{1a1}.py", "Balanceada_{0a1}_y_{1a0}.py", "Constante_{0a0}_y_{1a0}.py" y "Constante_{0a1}_y_{1a1}.py")
+
+### 3. Un archivo Qiskit.py 
+Este archivo fue un codigo de prueba dado en clase, el cual enseña como deberia (mas o menos) salir las funciones del punto numero 1.
+
+# Hecho con
+- Visual Studio Code (https://code.visualstudio.com)
+
+### Autor
+Juan Sebastian Buitrago Piñeros (Juanseproo)
+
+## Instalación
+1. Clona el repositorio en tu máquina local:
+git clone https://github.com/tu-nombre-de-usuario/pruebas-numeros-complejos.git
+
+2. Cambia al directorio del proyecto:
+
+3. Asegúrate de tener Python instalado. El código está escrito en Python 3.
